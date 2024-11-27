f8618cccc1e2f94980eafe3453d83836d0eae126
MiroTragico12 <jorge.santista10@hotmail.com>
2024-11-27 15:46:32 -0300

    test

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://3.15.225.151:8085'], 
+    'allowed_origins' => ['http://3.15.225.151:8000'], 
 
     'allowed_origins_patterns' => [],
 
